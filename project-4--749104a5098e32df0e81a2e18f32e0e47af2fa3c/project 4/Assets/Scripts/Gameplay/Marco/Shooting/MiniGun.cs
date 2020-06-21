fa9d8a039cf7cd40ff922f14d3e7be52c3ffe1d7
SleepyTabby <65219280+SleepyTabby@users.noreply.github.com>
2020-06-21 18:20:27 +0200

    .
    .

---
+++ project-4--749104a5098e32df0e81a2e18f32e0e47af2fa3c/project 4/Assets/Scripts/Gameplay/Marco/Shooting/MiniGun.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGun : MonoBehaviour
+{
+    [SerializeField] private Transform firePoint;
+    [SerializeField] Gun enable;
+    [Header("All Bullet prefabs")]
+    [SerializeField] private GameObject bulletFire;
+    
+    [SerializeField] private float fireSpeedFireBullet;
+    public bool pressurePlate;
+
+    public static MiniGun Instance;
+    float LastShot = 0f;
+    [Header("spray")]
+    [Range(0f, 5f)]
+    [SerializeField] float fireSpray;
+
+    void Awake()
+    {
+        Instance = GetComponent<MiniGun>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+
+
+    public void Shoot()
+    {
+        if (pressurePlate)
+        {
+            if (LastShot + fireSpeedFireBullet <= Time.time)
+            {
+                float randomSpray = Random.Range(firePoint.rotation.y - fireSpray, firePoint.rotation.y + fireSpray);
+                Quaternion spray = new Quaternion(firePoint.rotation.x, randomSpray, firePoint.rotation.z, firePoint.rotation.w);
+                LastShot = Time.time;
+                Instantiate(bulletFire, firePoint.position, spray);
+            }
+        }
+    }
+}
---
+++ project-4--749104a5098e32df0e81a2e18f32e0e47af2fa3c/project 4/Assets/Scripts/Gameplay/Marco/Shooting/MiniGunPressurePlate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGunPressurePlate : MonoBehaviour
+{
+    public MiniGun miniGun;
+    private void OnCollisionEnter(Collision col)
+    {
+        miniGun.pressurePlate = true;
+    }
+}
